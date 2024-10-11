83eb94e8cf188cc6d1d9012ff0febe941719fe14
Donnie Adams <donnie@acorn.io>
2024-10-10 20:20:29 -0400

    feat: add ability to ignore not found errors in CLI for rm commands
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/cli/rmfile.go
+++ pkg/cli/rmfile.go
@@ -1,13 +1,17 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/otto8-ai/workspace-provider/pkg/client"
 	"github.com/spf13/cobra"
 )
 
 type rmFile struct {
 	root *workspaceProvider
+
+	IgnoreNotFound bool `usage:"Ignore not found errors"`
 }
 
 func (r *rmFile) Customize(c *cobra.Command) {
@@ -20,6 +24,11 @@ func (r *rmFile) Run(cmd *cobra.Command, args []string) error {
 	workspaceID := args[0]
 	for _, arg := range args[1:] {
 		if err := r.root.client.DeleteFile(cmd.Context(), workspaceID, arg); err != nil {
+			var notFound *client.FileNotFoundError
+			if r.IgnoreNotFound && errors.As(err, &notFound) {
+				fmt.Printf("file %s not found in workspace %s\n", arg, workspaceID)
+				continue
+			}
 			return err
 		}
 
