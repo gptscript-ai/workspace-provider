401db4725b94a9fb1192f5149c656ad94d9f8683
Donnie Adams <donnie@acorn.io>
2024-10-28 07:44:56 -0400

    chore: add json tags to file info struct
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/client/fileinfo.go
+++ pkg/client/fileinfo.go
@@ -3,8 +3,8 @@ package client
 import "time"
 
 type FileInfo struct {
-	WorkspaceID string
-	Name        string
-	Size        int64
-	ModTime     time.Time
+	WorkspaceID string    `json:"workspaceID"`
+	Name        string    `json:"name"`
+	Size        int64     `json:"size"`
+	ModTime     time.Time `json:"modTime"`
 }
