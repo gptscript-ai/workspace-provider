23c552240d5ff8a17dd54cfde9163903f16aca2a
Donnie Adams <donnie@acorn.io>
2025-01-20 08:42:56 -0500

    fix: daemon support to create from workspace ids
    There are two issues here: it is not possible to specify an array
    for a tool argument. Secondly, it is nicer to specify a slice of
    strings instead of joining them. Therefore, the create workspace
    daemon server call will not support both methods.
    
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/server/create.go
+++ pkg/server/create.go
@@ -3,11 +3,15 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 type createRequest struct {
 	Provider         string   `json:"provider"`
 	FromWorkspaceIDs []string `json:"fromWorkspaceIDs"`
+	// This tool accepts two different types "from these workspaces" because it is not possible to specify that a tool
+	// argument is an array. So, we also support a comma-delimited string for workspace IDs.
+	WorkspaceIDs string `json:"workspace_ids"`
 }
 
 func (s *server) create(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +19,12 @@ func (s *server) create(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(err.Error()))
 		return
+	}
+
+	if req.WorkspaceIDs != "" {
+		req.FromWorkspaceIDs = append(req.FromWorkspaceIDs, strings.Split(req.WorkspaceIDs, ",")...)
 	}
 
 	id, err := s.client.Create(r.Context(), req.Provider, req.FromWorkspaceIDs...)
