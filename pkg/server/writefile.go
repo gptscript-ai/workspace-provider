2e383182390a8ffc94dac89436272b36ecf0678b
Donnie Adams <donnie@acorn.io>
2025-01-27 08:00:28 -0500

    enhance: update daemon server to support the disabling of revisions
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/server/writefile.go
+++ pkg/server/writefile.go
@@ -4,13 +4,21 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
+
+	"github.com/gptscript-ai/workspace-provider/pkg/client"
 )
 
 func (s *server) writeFile(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 	fileName := r.PathValue("fileName")
+	query := r.URL.Query()
 
-	if err := s.client.WriteFile(r.Context(), id, fileName, base64.NewDecoder(base64.StdEncoding, r.Body)); err != nil {
+	opts := client.WriteOptions{}
+	if query.Get("createRevision") == "false" {
+		opts.CreateRevision = new(bool)
+	}
+
+	if err := s.client.WriteFile(r.Context(), id, fileName, base64.NewDecoder(base64.StdEncoding, r.Body), opts); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write([]byte(err.Error()))
 		return
